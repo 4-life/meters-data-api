89ed345e67f9fc8682b0e8d521d0c01306ba2b1d
4life <p.ovchinnikov@adguard.com>
2020-04-21 20:01:53 +0300

    Fix connect to db

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,15 @@
 require('dotenv').config();
 import { IEventContext, IOptions } from 'pg-promise';
 
+const endpoint: string = process.env.ENDPOINT || '';
+const user: string     = process.env.DB_USER || '';
+const password: string = process.env.PASSWORD || '';
+const port: string     = process.env.PORT || '';
+const dbName: string   = process.env.DB_NAME || '';
+const DEBUG = !!process.env.DEBUG;
+
 const LOG = {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   db: require('debug')('db')
 };
 
@@ -11,7 +19,13 @@ const options: IOptions<object> = {
   }
 };
 
-const pgp = require('pg-promise')(options);
-const db = pgp(process.env.DATABASE_URL);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pgp = require('pg-promise')(DEBUG ? options : {});
+
+const dbUrl = `postgres://${user}:${password}@${endpoint}:${port}/${dbName}`;
+
+LOG.db(dbUrl);
+
+const db = pgp(dbUrl);
 
 export default db;
